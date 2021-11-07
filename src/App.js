e585480d7ad4b74881c484b1f77860d39b057f6c
Deepak0113 <mdeecreations@gmail.com>
2021-11-07 12:59:54 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from './actions'
+
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      working
+      <h1>Counter : {counter}</h1>
+      <button onClick={() => dispatch(increment(10))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {
+        isLogged ?
+        <h3>Valuable Information</h3> :
+        <h3>cant see</h3>
+      }
     </div>
   );
 }
